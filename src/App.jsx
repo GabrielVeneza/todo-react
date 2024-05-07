9c033c835b7afb537ed6dc8c885bfd835755fcb2
Gabriel Oliveira <gabriel_mrsonus@outlook.com>
2024-05-07 00:45:04 -0300

    ate o minuto 30 da aula

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 import { useState } from 'react'
+import react from "react";
+import Todo from './components/Todo'
+import TodoForm from './components/TodoForm'
 
+import "./App.css"
 
 function App() {
 
@@ -30,17 +34,10 @@ function App() {
       <h1>Lista de Tarefas</h1>
       <div className="todo-list">
         {todos.map((todo) => (
-          <div className="todo">
-            <div className="content">
-              <p>{todo.text}</p>
-              <p className="category">({todo.category})</p>
-            </div>
-            <div>
-              <button></button>
-            </div>
-          </div>
+          <Todo todo={todo} />
         ))}
       </div>
+      <TodoForm />
     </div>
   )
 }
---
+++ src/components/TodoForm.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { useState } from 'react'
+
+
+const TodoForm = () => {
+
+    const [value, setValue] = useState("");
+    const [category, setCategory] = useState("");
+
+    const handlesubmit = (e) => {
+        e.preventDefault();
+        console.log(value, category)
+    }
+
+  return (
+    <div className='todo-form'>
+        <h2>Criar tarefa:</h2>
+        <form onSubmit={handlesubmit}>
+            <input 
+                type="text" 
+                placeholder='Digite o título...' 
+                onChange={(e) => setValue(e.target.value)}
+            />
+            <select onChange={(e) => setCategory(e.target.value)}>
+                <option value="">Selecione uma categoria</option>
+                <option value="Trabalho">Trabalho</option>
+                <option value="Pessoal">Pessoal</option>
+                <option value="Estudos">Estudos</option>
+            </select>
+            <button type="submit">Criar tarefa</button>
+        </form>
+    </div>
+  )
+}
+
+export default TodoForm
